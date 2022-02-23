a66b2c5e5a425b4956e646a6d64cce7e397b70b8
varun174 <42437402+varun174@users.noreply.github.com>
2022-02-23 12:36:09 +0530

    added calculator files
    added calculator files

---
+++ cal.js
@@ -0,0 +1,26 @@
+function add()
+         {
+             let x = parseInt(document.getElementById("n").value);
+             let y = parseInt(document.getElementById("m").value);
+              document.getElementById("result").innerHTML= x + y;
+			  console.log(res);
+         }
+function sub()
+         {
+             let x = parseInt(document.getElementById("n").value);
+             let y = parseInt(document.getElementById("m").value);
+              document.getElementById("result").innerHTML= x - y;
+         }
+function mul()
+         {
+             let x = parseInt(document.getElementById("n").value);
+             let y = parseInt(document.getElementById("m").value);
+              document.getElementById("result").innerHTML= x * y;
+         }
+function div()
+         {
+             let x = parseInt(document.getElementById("n").value);
+             let y = parseInt(document.getElementById("m").value);
+              document.getElementById("result").innerHTML= x / y;
+         }
+
